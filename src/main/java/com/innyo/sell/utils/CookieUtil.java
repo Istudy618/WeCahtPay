3d26cb4f96dfdffb91a3480b88d467a86704e442
Istudy618 <53629882@qq.com>
2018-04-23 13:51:34 +0800

    实现系统的登出

--- src/main/java/com/innyo/sell/utils/CookieUtil.java
+++ src/main/java/com/innyo/sell/utils/CookieUtil.java
@@ -1,7 +1,10 @@
 package com.innyo.sell.utils;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: XM
@@ -29,7 +32,34 @@ public class CookieUtil {
 
     }
 
-    public static void get(){
+    /**
+     * 获取cookie
+     * @param request
+     * @param name
+     * @return
+     */
+    public static Cookie get(HttpServletRequest request,String name){
+            Map<String,Cookie> cookieMap = redCookieMap(request);
+            if (cookieMap.containsKey(name)){
+                return cookieMap.get(name);
+            }else {
+                return null;
+            }
+    }
 
+    /**
+     * 将cookie封装成map
+     * @param request
+     * @return
+     */
+    private static Map<String,Cookie> redCookieMap(HttpServletRequest request){
+            Map<String,Cookie> cookieMap = new HashMap<>();
+            Cookie[] cookies = request.getCookies();
+            if(cookies !=null){
+                for (Cookie cookie:cookies){
+                    cookieMap.put(cookie.getName(),cookie);
+                }
+            }
+            return cookieMap;
     }
 }
